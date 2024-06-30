0c8a241610742d657437f8e0123e499b07dc36f8
Mikel Bober-Irizar <mikel@mxbi.net>
2024-06-30 19:11:28 +0100

    New dataset versions, packaging and refactoring

--- arckit/data.py
+++ arckit/data.py
@@ -40,8 +40,9 @@ def fmt_grid(grid, colour=True, spaces=True):
         return Text.assemble(*grid_str[:-1])
 
 class Task:
-    def __init__(self, id, train, test, dataset=None):
+    def __init__(self, id, train, test, dataset=None, version=None):
         self.dataset = dataset
+        self.version = version
         self.id = id
         self.train = [(np.array(example['input']), np.array(example['output'])) for example in train]
         self.test = [(np.array(example['input']), np.array(example['output'])) for example in test]
@@ -238,54 +239,63 @@ class TaskSet:
         else:
             return total_score
 
-def load_data(version='latest') -> (TaskSet, TaskSet):
+def get_data_json(version):
+    if version in ['latest', 'arcagi', 'aa922be']:
+        return json.load(open(f"{os.path.dirname(__file__)}/data/arcagi_aa922be.json"))
+    
+    elif version in ['kaggle', 'kaggle2024']:
+        return json.load(open(f"{os.path.dirname(__file__)}/data/kaggle2024.json"))
+    
+    elif version in ['arc', 'kaggle2019']:
+        return json.load(open(f"{os.path.dirname(__file__)}/data/arc1.json"))
+    
+    else:
+        raise ValueError(f"Unknown ARC dataset version: {version}")
 
-    if version == 'latest':
-        data = json.load(open(f"{os.path.dirname(__file__)}/data/arcagi.json"))       
-    elif version =='kaggle':
-        data = json.load(open(f"{os.path.dirname(__file__)}/data/kaggle.json"))
-    elif version == 'arc':
-        data = json.load(open(f"{os.path.dirname(__file__)}/data/arc1.json"))
+def load_data(version='latest') -> (TaskSet, TaskSet):
+    """
+    Load the ARC dataset from disk. Optionally, specify a specific version of the dataset to load.
+    """
+
+    data = get_data_json(version)
         
     train_tasks = []
     eval_tasks = []
     for id, task in data['train'].items():
-        train_tasks.append(Task(id, task['train'], task['test'], 'train'))
+        train_tasks.append(Task(id, task['train'], task['test'], 'train', version=version))
 
     for id, task in data['eval'].items():
-        eval_tasks.append(Task(id, task['train'], task['test'], 'eval'))
+        eval_tasks.append(Task(id, task['train'], task['test'], 'eval', version=version))
 
     return TaskSet(train_tasks), TaskSet(eval_tasks)
 
 
 def load_single(id: str, version='latest') -> Task:
     """
-    Load a single task from disk.
+    Load a single task from disk. IDs are of the form 'train0', 'eval14', '007bbfb7', etc.
+    Note that if iterating through tasks, it is more efficient to use load_data() and index into it.
+
+    Optionally, specify a specific version of the dataset to load.
     """
 
-    if version == 'arc':
-        data = json.load(open(f"{os.path.dirname(__file__)}/data/arc1.json"))
-    elif version =='latest':
-        data = json.load(open(f"{os.path.dirname(__file__)}/data/arcagi.json"))
-    elif version =='kaggle':
-        data = json.load(open(f"{os.path.dirname(__file__)}/data/kaggle.json"))
+    data = get_data_json(version)
         
     # task = data['train'][id]
     # return Task(id, task['train'], task['test'], 'train'
     if id.startswith('train'):
         dataset_tasks = sorted(data['train'].items())
         taskid, task = dataset_tasks[int(id[5:])]
-        return Task(taskid, task['train'], task['test'], 'train')
+        return Task(taskid, task['train'], task['test'], 'train', version=version)
     elif id.startswith('eval'):
         dataset_tasks = sorted(data['eval'].items())
         taskid, task = dataset_tasks[int(id[4:])]
-        return Task(taskid, task['train'], task['test'], 'eval')
+        return Task(taskid, task['train'], task['test'], 'eval', version=version)
     elif id in data['train']:
         task = data['train'][id]
-        return Task(id, task['train'], task['test'], 'train')
+        return Task(id, task['train'], task['test'], 'train', version=version)
     elif id in data['eval']:
         task = data['eval'][id]
-        return Task(id, task['train'], task['test'], 'eval')
+        return Task(id, task['train'], task['test'], 'eval', version=version)
     else:
         raise ValueError(f"Unknown task id: {id}")
 
